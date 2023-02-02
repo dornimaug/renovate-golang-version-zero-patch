81d3674e495b10b7d2dcd8bc07170cada55a10e3
Klaus Dorninger <klaus.dorninger@itsv.at>
2023-02-02 11:49:42 +0100

    Use 1.19.5 in master as this should actually work

--- reproduce.sh
+++ reproduce.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # !renovate depName=go datasource=golang-version
-GOLANG_VERSION="1.19.0"
+GOLANG_VERSION="1.19.5"
 
-curl -fsSL "https://go.dev/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz" | tar -C ./build -xzf - \
-   && ./build/go/bin/go version
+mkdir -p build
+
+curl -fsSL "https://go.dev/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz" | tar -C build -xzf - \
+   && build/go/bin/go version
